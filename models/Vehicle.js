1d652acb6aafd40eb41001716ddf142f50e45e1c
rushikeshtaur12 <rushikeshtaur12@gmail.com>
2025-09-04 22:23:18 +0530

    chnages

--- models/Vehicle.js
+++ models/Vehicle.js
@@ -4,7 +4,9 @@ const vehicleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   capacityKg: { type: Number, required: true },
   tyres: { type: Number, required: true },
-  booked: { type: Boolean, default: false }  // key to track booking status
+  booked: { type: Boolean, default: false }, // key to track booking status
+  createdAt: { type: Date, default: Date.now }, // ← make sure this exists
+
 });
 
 export default mongoose.model("Vehicle", vehicleSchema);
